c2b355f040ce5d79cc750d99816b93b6ea1c88b9
hazemelraffiee <hazem.farouk.elraffiee@gmail.com>
2024-12-31 20:51:50 +0100

    try a different deployment strategy

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     // We'll only use these settings when we're ready to deploy
     ...(process.env.NODE_ENV === 'production' ? {
       output: 'export',
-      basePath: '/weekly-message-generator',
-      assetPrefix: '/weekly-message-generator',
+      basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+      assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
     } : {}),
     images: {
       unoptimized: true,
