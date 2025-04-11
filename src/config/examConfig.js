f2665eb05fb349e4c1e4c89de2be8948406f1217
hazemelraffiee <hazem.farouk.elraffiee@gmail.com>
2025-04-11 18:57:11 +0200

    Update exam weights

--- src/config/examConfig.js
+++ src/config/examConfig.js
@@ -8,10 +8,45 @@ export const examConfigs = {
   "الفوج التمهيدي - القاعدة النورانية": {
     examName: "اختبار الفصل الدراسي الثاني",
     sections: [
-      { id: "memorization", name: "حفظ", weight: 2 },
+      { id: "memorization", name: "حفظ", weight: 3 },
       { id: "tajweed", name: "تجويد", weight: 2 },
       { id: "nouraneya", name: "القاعدة النورانية", weight: 3 },
       { id: "extra_memorization", name: "حفظ إضافي - الحزب الـ 60", weight: 3 },
+    ]
+  },
+
+  "الفوج الأول": {
+    examName: "اختبار الفصل الدراسي الثاني",
+    sections: [
+      { id: "memorization", name: "حفظ", weight: 7 },
+      { id: "tajweed", name: "تجويد", weight: 2 },
+      { id: "performance", name: "أداء", weight: 1 }
+    ]
+  },
+
+  "الفوج الثاني": {
+    examName: "اختبار الفصل الدراسي الثاني",
+    sections: [
+      { id: "memorization", name: "حفظ", weight: 5 },
+      { id: "revision", name: "مراجعة", weight: 3.5 },
+      { id: "tajweed", name: "تجويد", weight: 1.5 }
+    ]
+  },
+
+  "الفوج الرابع": {
+    examName: "اختبار الفصل الدراسي الثاني",
+    sections: [
+      { id: "memorization", name: "حفظ", weight: 4 },
+      { id: "revision", name: "مراجعة", weight: 3 },
+      { id: "tajweed", name: "تجويد", weight: 2 },
+    ]
+  },
+
+  "الفوج الخامس": {
+    examName: "اختبار الفصل الدراسي الثاني",
+    sections: [
+      { id: "memorization", name: "حفظ", weight: 3 },
+      { id: "revision", name: "مراجعة", weight: 2 }
     ]
   },
 
@@ -20,7 +55,8 @@ export const examConfigs = {
     examName: "اختبار الفصل الدراسي الثاني",
     sections: [
       { id: "memorization", name: "حفظ", weight: 3 },
-      { id: "tajweed", name: "تجويد", weight: 1 },
+      { id: "revision", name: "مراجعة", weight: 2 },
+      { id: "tajweed", name: "تجويد", weight: 2 },
     ]
   }
 };
